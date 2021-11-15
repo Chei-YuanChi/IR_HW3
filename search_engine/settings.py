da5ed6943cf2677f0499e0dd827d9a6a592bf9e3
Chei-YuanChi <87289035+Chei-YuanChi@users.noreply.github.com>
2021-11-15 18:22:57 +0800

    Update settings.py

--- search_engine/settings.py
+++ search_engine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r2)w1exg6!xsn99krq$#%li01stcg16545ixh&(iucnp0lbwol'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
